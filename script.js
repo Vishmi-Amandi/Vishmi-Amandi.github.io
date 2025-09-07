6b968ce4f90c637747eb6df7ac5bb38cc40d9601
Vishmi Amandi <vishwijesooriya@gmail.com>
2025-09-07 23:54:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
